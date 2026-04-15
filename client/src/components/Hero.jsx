f4d8a8d5f5d5a6d644dea726ac6ce9540a1c1de8
Pratik Wani <pwani8306@gmail.com>
2026-04-15 09:59:03 +0530

    feat: add hero section component

---
+++ client/src/components/Hero.jsx
@@ -0,0 +1,85 @@
+import heroImg from "../assets/r2.0_logo.png";
+import { motion } from "framer-motion";
+import ShinyText from "./ui/ShinyText"; 
+import { Sparkles } from "lucide-react";
+
+function Hero() {
+  return (
+    <section className="flex flex-col md:flex-row items-center justify-between max-w-6xl mx-auto px-6 py-10 md:py-16 gap-12">
+      
+      {/* Left Content */}
+      <motion.div
+        initial={{ opacity: 0, y: 40 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1 }}
+        className="max-w-xl text-center md:text-left"
+      >
+        <div className="inline-flex items-center gap-2 bg-[#ffd6e5] px-4 py-2 rounded-full mb-3">
+          
+          <div className="flex items-center gap-2">
+          <Sparkles size={16} className="text-pink-500" />
+  
+          <ShinyText
+          text="Trusted by 20,000+ Women"
+          className="text-sm font-semibold"
+          speed={4}
+          delay={0}
+          color="#e91e63"
+          shineColor="#ffffff"
+          spread={120}
+          pauseOnHover={false}
+          />
+        </div>
+
+        </div>
+
+        {/* Heading */}
+        <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold leading-tight mb-6">
+          Your Personal Smart <br />
+          <span className="text-[#e91e63] audiowide">
+            Safety Protection
+          </span>{" "}
+          Companion.
+        </h1>
+
+        {/* Description */}
+        <p className="text-gray-600 mb-8 text-sm sm:text-base leading-relaxed">
+          A smart A.I based platform for women to report safety incidents,
+          send emergency SOS alerts and track complaint status securely.
+        </p>
+
+        {/* Buttons */}
+        <div className="flex flex-col sm:flex-row gap-4 justify-center md:justify-start font-semibold">
+          <button onClick={() => {
+           document.getElementById("why-raksha")?.scrollIntoView({ behavior: "smooth" });
+        }}
+        className="bg-[#e91e63] text-white px-6 py-3 rounded-full hover:scale-105 transition-all duration-300 shadow-md">
+        Start ➜
+        </button>
+
+          <button className="border-2 border-[#e91e63] text-[#e91e63] px-6 py-3 font-semibold rounded-full hover:bg-[#e91e63] hover:text-white transition-all duration-300">
+            Safety Guide
+          </button>
+        </div>
+      </motion.div>
+
+      {/* Right Image */}
+      <motion.div
+        initial={{ opacity: 0, y: 40 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1, delay: 0.2 }}
+        className="hidden md:flex justify-center"
+      >
+        <motion.img
+          src={heroImg}
+          alt="Raksha Safety Hero"
+          className="w-110 drop-shadow-2xl"
+          whileHover={{ scale: 1.05 }}
+        />
+      </motion.div>
+
+    </section>
+  );
+}
+
+export default Hero;
